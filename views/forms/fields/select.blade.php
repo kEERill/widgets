cf184f99ab2b955cdf50ad0d0a75e40dcf360829
kEERill <russia199797@mail.ru>
2019-01-25 16:12:56 +0300

    Update select.blade.php

--- views/forms/fields/select.blade.php
+++ views/forms/fields/select.blade.php
@@ -5,7 +5,7 @@
         'formField' => $formField
     ])
 
-    <?= Form::select($formField->getNameToHtml() . '[]', $formField->getSelectOptions(), $formField->getValue(), $formField->getInputAttributes()) ?>
+    <?= Form::select($formField->getNameToHtml(), $formField->getSelectOptions(), $formField->getValue(), $formField->getInputAttributes()) ?>
 
     @include('widgets::forms.comment', [
         'formField' => $formField
