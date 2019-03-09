4a39cafb9707a468a3e916e4969d7faa9240f6f0
kEERill <russia199797@mail.ru>
2019-03-10 00:23:20 +0300

    [fix] Исправлена ошибка при проверке выбранных вариантов для multiselect

--- src/Forms/Types/MultiSelectType.php
+++ src/Forms/Types/MultiSelectType.php
@@ -1,5 +1,7 @@
 <?php namespace Keerill\Widgets\Forms\Types;
 
+use Illuminate\Support\Collection;
+
 class MultiSelectType extends SelectType
 {
     /**
@@ -25,26 +27,21 @@ class MultiSelectType extends SelectType
     }
 
     /**
-     * Возвращает атрибуты для input
-     * @return array
-     */
-    public function getInputAttributes()
-    {
-        $baseClasses = parent::getInputAttributes();
-        $baseClasses[] = 'multiple';
-
-        return $baseClasses;
-    }
-
-    /**
      * Возвращает true если элемент key выбран
      * @param mixed $key
      * @return bool
      */
     public function isSelect($key)
     {
-        if (is_array($value = request()->old($this->getName(), $this->getValue())))
-            return in_array($key, $value);
+        if ($value = request()->old($this->getName(), $this->getValue())) {
+            if (is_array($value))
+                return in_array($key, $value);
+
+            if ($value instanceof Collection)
+                return $value->contains($key);
+
+            return false;
+        }
 
         return false;
     }
